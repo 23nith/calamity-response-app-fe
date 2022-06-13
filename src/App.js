574182863ffc5a644e34dd5d50feeb922758bd5b
23nith <user.name>
2022-06-13 16:05:01 +0800

    Fix react route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {HashRouter as Router, Switch, Route} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import Success from './components/success';
 import Failed from './components/failed';
 
@@ -12,12 +12,10 @@ function App() {
               Home
             </div>
           </Route>
-          <Route exact path="/success" component={Success}>
-            <Success/>
-          </Route>
-          <Route exact path="/failed" component={Failed}>
-            <Failed/>
-          </Route>
+          <Route exact path="/success" component={Success} />
+            
+          <Route exact path="/failed" component={Failed} />
+            
         </Switch>
     // </Router>
   );
