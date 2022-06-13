d95ff54486850ed8b5595e36529e87621be56b5c
23nith <user.name>
2022-06-13 15:52:09 +0800

    Change BrowserRouter with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 import Success from './components/success';
 import Failed from './components/failed';
 
