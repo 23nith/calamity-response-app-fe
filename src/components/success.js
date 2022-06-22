7ef350f14d6a4201babb6e4193ef4eab60fae599
23nith <user.name>
2022-06-22 22:27:41 +0800

    Add top navbar

--- src/components/success.js
+++ src/components/success.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Success() {
   return (
-    <div className="p-7 text-2xl font-semibold flex-1 h-screen">
+    <div className="p-7 text-2xl font-semibold flex-1">
       <div className='App'>Success</div>
     </div>
   )
