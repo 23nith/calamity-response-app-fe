443761e1953d40c63e24747bb21d4e0fe691ffe6
23nith <user.name>
2022-06-22 14:26:16 +0800

    Remove sidebar when showing login

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import Success from './components/success';
 import Failed from './components/failed';
 import Sidebar from './components/sidebar';
 import Sign_in from './components/sign_in';
+import { useState } from 'react';
 
 function App() {
+  const [showLogin, setShowLogin] = useState(false)
+  const [showSignUp, setShowSignUp] = useState(true)
+  
   return (
-        <div className='flex'>
-          <Sidebar/>
-          <div className="p-7 text-2xl font-semibold flex-1 h-screen">
-              <Route exact path="/login" component={Sign_in} />
-              <Route exact path="/success" component={Success} />
-              <Route exact path="/failed" component={Failed} />
-          </div>
-        </div>
+        <>
+          {showLogin && <Route exact path="/" component={Sign_in} />}
+          {!showLogin && <div className='flex'>
+            <Sidebar/>
+            <div className="p-7 text-2xl font-semibold flex-1 h-screen">
+                <Route exact path="/success" component={Success} />
+                <Route exact path="/failed" component={Failed} />
+            </div>
+          </div>}
+        </>
   );
 }
 
