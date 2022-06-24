1b99a3fa00604d6b5f9ebd5cc66115676783cace
23nith <user.name>
2022-06-24 13:28:18 +0800

    Add dynamic username value at the top-right of navbar

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import Failed from "./components/failed";
 import Sidebar from "./components/sidebar";
 import Sign_in from "./components/sign_in";
 import Sign_up from "./components/sign_up";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { BiUserCircle } from "react-icons/bi";
-import UserContextProvider from "./contexts/UserContext";
+import UserContextProvider, { UserContext } from "./contexts/UserContext";
 import AreasContextProvider from "./contexts/AreasContext";
 
 function App() {
   const [showLogin, setShowLogin] = useState(true);
   const [showSignUp, setShowSignUp] = useState(false);
+  const {currentUser} = useContext(UserContext);
 
   return (
     <>
       <AreasContextProvider>
-        <UserContextProvider>
           {showLogin && (
             <Route
               exact
@@ -52,7 +52,7 @@ function App() {
                 <div className="border-b-2 border-b-grey flex flex-row justify-end">
                   <div className="px-5 py-2 flex flex-row">
                     <div className="flex flex-col justify-end text-dark-grey">
-                      <div className="text-md text-end">User Name</div>
+                      <div className="text-md text-end">{currentUser.first_name}</div>
                       <div className="text-sm text-end">account settings</div>
                     </div>
 
@@ -70,7 +70,6 @@ function App() {
               </div>
             </div>
           )}
-        </UserContextProvider>
       </AreasContextProvider>
     </>
   );
