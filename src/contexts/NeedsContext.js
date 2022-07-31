a2face318901196ef0c8955e54827c5cfd19d89a
23nith <user.name>
2022-08-01 03:45:51 +0800

    Add real-time update to add/edit/delete need feature

--- src/contexts/NeedsContext.js
+++ src/contexts/NeedsContext.js
@@ -7,8 +7,29 @@ export const NeedsContext = createContext();
 function NeedsContextProvider(props) {
   const [needs, setNeeds] = useState([]);
 
+  const updateNeeds = () => {
+    fetch("http://localhost:3000/needs", {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res)=>{
+        if(res.ok){
+          return res.json()
+        }else{
+          throw new Error(res)
+        }
+      })
+      .then((data)=>{
+        console.log("areas data: ", data)
+        setNeeds(data)
+      })
+  }
+
+
   return (
-    <NeedsContext.Provider value={{needs, setNeeds}}>
+    <NeedsContext.Provider value={{needs, setNeeds, updateNeeds}}>
       {props.children}
     </NeedsContext.Provider>
   )
